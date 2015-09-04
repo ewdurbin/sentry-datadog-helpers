e092a033944986730b71a0963b4837564d6cf0d1
Ernest W. Durbin III <ewdurbin@gmail.com>
2015-09-03 22:41:33 -0400

    break up raven and sentry dependencies

--- setup.py
+++ setup.py
@@ -6,14 +6,19 @@ An suite of Sentry tools which enhance integrations with Datadog
 """
 from setuptools import setup
 
-install_requires = [
+install_requires = []
+
+raven_requires = [
     'raven>=5.6.0',
+]
+
+sentry_requires = [
     'sentry>=7.4.0',
 ]
 
 tests_requires = [
     'mock==1.3.0',
-]
+] + (raven_requires + sentry_requires)
 
 setup(
     name='sentry-datadog-helpers',
@@ -27,6 +32,10 @@ setup(
         'sentry_datadog_helpers',
         'sentry_datadog_helpers.raven',
     ],
+    extras_requires={
+        'raven': raven_requires,
+        'sentry': sentry_requires,
+    },
     license='BSD',
     zip_safe=False,
     install_requires=install_requires,
